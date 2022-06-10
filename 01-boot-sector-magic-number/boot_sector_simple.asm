b22fc87471a30069f46548dda065830f7ab9164b
Adstefnum <bauerstevenson@gmail.com>
2022-06-11 01:44:44 +0300

    assembly code for  boot sector of an OS

---
+++ 01-boot-sector-magic-number/boot_sector_simple.asm
@@ -0,0 +1,15 @@
+;this is an emulation of a boot sector read by the BIOS
+;it however just loops forever after reading the 0xaa55 magic number
+;compile with nasm -f bin boot_sector_simple.asm -o boot_sector_simple.bin
+;run with qemu boot_sector_simple.bin
+
+;runs an infinite loop with the manufacture instruction, e9 fd ff
+loop:
+	jmp loop
+
+;fill with x zeros where x max block size(512) - size of magic number (2) - space for
+;previous instruction
+times 510 - ($-$$) db 0
+
+;add the magic number
+dw 0xaa55
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RhemaOS
 A simple operating system built from scratch
+
+# References
+- [Github tutorial based on the book below](https://github.com/cfenollosa/os-tutorial)
+- [Make a simple OS from scratch](http://www.cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf)
