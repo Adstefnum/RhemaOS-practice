b6b3970c5676d4a993ce3e65727725e93353fd88
Adstefnum <bauerstevenson@gmail.com>
2022-06-11 21:57:28 +0300

    corrected comment

--- 03-memory-addressing/org-7000-mem-address.asm
+++ 03-memory-addressing/org-7000-mem-address.asm
@@ -1,8 +1,8 @@
 ;this program simply finds where a variable is stored in memory
 ;to demonstrate memory addressing by retrieving the variable 
 ;stored in memory
-;this uses the org convention to specify the start of what? 
-;it is inconvinient
+;this uses the org convention to specify the start of the memory location that
+;the boot loader loaded us to  it is inconvinient
 
 [org 0x7c00]
 
